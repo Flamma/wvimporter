d5829c76d6a4f46add162d758c6d35672a5c60e9
Pablo J. Urbano Santos <flamma@member.fsf.org>
2019-07-28 19:20:19 +0200

    feat: convert html code to bbcode.

--- process/mia_bb_transformer.php
+++ process/mia_bb_transformer.php
@@ -1,7 +1,11 @@
 <?php
 
+require_once '../vendor/autoload.php';
+
 function transform_content($content) {
     $result = add_class_quote($content);
+    $result = replace_no_href_links($result); // Due to a bug on HTMLConverter
+    $result = html_to_bbcode($result);
 
     return $result;
 }
@@ -16,4 +20,29 @@ function add_class_quote($content){
     }
 </style>
     '.$content;
+}
+
+function html_to_bbcode($content) {
+    $converter = new Converter\HTMLConverter($content);
+
+    try {
+        $result = $converter->toBBCode();
+    } catch (Exception $e) {
+        print("\nExcepción $e capturada con el contenido:");
+        print("\n$content");
+        throw $e;
+    }
+
+    return $result;
+}
+
+function replace_no_href_links($content) {
+    preg_match_all("/<a([^>]*)>/", $content, $matches, PREG_SET_ORDER);
+
+    foreach($matches as $matching) {
+        if(strpos($matching[1], "href") === FALSE)
+            $content = str_replace($matching[0], '<a href="#" '.$matching[1].'>', $content);
+    }
+
+    return $content;
 }
