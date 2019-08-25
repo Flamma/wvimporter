0a26619e12d34c57d1f1535b910eff106d6c2169
Pablo J. Urbano Santos <flamma@member.fsf.org>
2019-08-25 16:24:09 +0200

    feat: cli now accepts a list of files as input.

--- cli/import.php
+++ cli/import.php
@@ -4,22 +4,39 @@ require_once('../process/parser.php');
 require_once('../process/importer.php');
 
 if($argc < 2) {
-    die("You must specify the file to be imported.\n");
+    die("You must specify the files to be imported.\n");
 }
 
-$filename = $argv[1];
+$error_files = Array();
 
-if(!file_exists($filename)) {
-    die("Cannot find $filename\n");
+$filenames = array_slice($argv, 1);
+
+foreach($filenames as $filename) {
+    print("Importing '$filename'...\n");
+
+    if(!file_exists($filename)) {
+        $error_files[] = $filename;
+        print("Cannot find '$filename'\n");
+    }
+
+    $content = file_get_contents($filename);
+
+    if(!strlen($content) > 0) {
+        $error_files = $filename;
+        print("'$filename' is empty\n");
+    }
+
+    $subforum = parse($content);
+
+    import($subforum);
 }
-
-$content = file_get_contents($filename);
-
-if(!strlen($content) > 0)
-    die('File is empty');
-
-$subforum = parse($content);
-
-import($subforum);
 
 echo "DONE\n";
+
+if(count($error_files) > 0) {
+    print("The following files could not be imported: \n");
+    foreach($error_files as $error_file)
+        print("\t$error_file\n");
+
+    die();
+}
